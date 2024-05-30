b2f8be7d333e8cb28b14aa27f215119dd796467a
garret14deras <139816612+garret14deras@users.noreply.github.com>
2024-05-30 03:27:02 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,17 @@
+}
+    const data = [];
+        console.log("Random Number: " + item);
+    }
+    return data;
+    });
+
+
+
+    for (let i = 0; i < 10; i++) {
+}
+function generateRandomData() {
+function main() {
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+    const data = generateRandomData();
